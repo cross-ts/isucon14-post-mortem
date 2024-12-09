c2bc9098a7e48c6557c45859e1a177629f4b2cf9
cross-ts <ts.kouhei@gmail.com>
2024-12-10 00:05:15 +0900

    [588] アクセスログ解析の調整(初期スコア)

--- duckdb/macros.sql
+++ duckdb/macros.sql
@@ -1,6 +1,10 @@
 create or replace macro req2path(req) as (
   split_part(req, ' ', 2)
-    .regexp_replace('\?.+$', '?<query>')
-    .regexp_replace('[0-9]+', '[0-9]+')
-    .regexp_replace('\@[a-z]+', '@[a-z]+')
+    .regexp_replace('[0-9a-z]{26}', '[ulid]', 'i')
+    .regexp_replace('/.+\.png$', '/[image].png', 'i')
+    .regexp_replace('/.+\.svg$', '/[image].svg', 'i')
+    .regexp_replace('/.+\.ico$', '/[favicon].ico', 'i')
+    .regexp_replace('/.+\.js$', '/[script].js', 'i')
+    .regexp_replace('/.+\.css$', '/[style].css', 'i')
+    .regexp_replace('-?[0-9]+', '[num]', 'g')
 );
