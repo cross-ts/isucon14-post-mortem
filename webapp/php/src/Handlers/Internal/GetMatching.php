c5ae0effaf568e6f2ee9f3ff6237030bed31d54c
cross-ts <ts.kouhei@gmail.com>
2024-12-11 00:51:19 +0900

    [24497] pick場所に一番近い椅子を送配する

--- webapp/php/src/Handlers/Internal/GetMatching.php
+++ webapp/php/src/Handlers/Internal/GetMatching.php
@@ -8,6 +8,7 @@ use IsuRide\Handlers\AbstractHttpHandler;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use PDO;
+use PDOException;
 
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 class GetMatching extends AbstractHttpHandler
@@ -28,32 +29,59 @@ class GetMatching extends AbstractHttpHandler
             return $this->writeNoContent($response);
         }
 
+        $stmt = $this->db->prepare(<<<SQL
+SELECT *
+FROM chairs c
+WHERE c.is_active = TRUE
+AND NOT EXISTS (
+    SELECT 1
+    FROM (
+        SELECT COUNT(rs.chair_sent_at) = 6 AS completed
+        FROM rides r
+        JOIN ride_statuses rs ON r.id = rs.ride_id
+        WHERE r.chair_id = c.id
+        GROUP BY rs.ride_id
+    ) is_completed
+    WHERE completed = FALSE
+);
+SQL
+        );
+        $stmt->execute();
+        $availableChairs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if (count($availableChairs) === 0) {
+            return $this->writeNoContent($response);
+        }
+
         foreach ($waitingRides as $ride) {
-            for ($i = 0; $i < 10; $i++) {
-                $stmt = $this->db->prepare(
-                    'SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1'
-                );
-                $stmt->execute();
-                $matched = $stmt->fetch(PDO::FETCH_ASSOC);
-                if (!$matched) {
-                    return $this->writeNoContent($response);
-                }
-                $stmt = $this->db->prepare(
-                    'SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE'
-                );
-                $stmt->execute([$matched['id']]);
-                $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                $empty = $result['COUNT(*) = 0'];
-                if ($empty) {
-                    break;
-                }
+            if (count($availableChairs) === 0) {
+                break;
             }
-            if (!$empty) {
-                return $this->writeNoContent($response);
-            }
+            $matched = $this->findMatchedChair($ride, $availableChairs);
+
             $stmt = $this->db->prepare('UPDATE rides SET chair_id = ? WHERE id = ? AND chair_id IS NULL');
-            $stmt->execute([$matched['id'], $ride['id']]);
+            try {
+                $stmt->execute([$matched['id'], $ride['id']]);
+            } catch (PDOException) {
+                continue;
+            }
         }
         return $this->writeNoContent($response);
+    }
+
+    private function findMatchedChair(array $ride, array &$availableChairs): array
+    {
+        $minDistance = PHP_INT_MAX;
+        $matched = null;
+        $key = null;
+        foreach ($availableChairs as $index => $chair) {
+            $distance = abs($ride['pickup_latitude'] - $chair['latitude']) + abs($ride['pickup_longitude'] - $chair['longitude']);
+            if ($distance < $minDistance) {
+                $minDistance = $distance;
+                $matched = $chair;
+                $key = $index;
+            }
+        }
+        unset($availableChairs[$key]);
+        return $matched;
     }
 }
