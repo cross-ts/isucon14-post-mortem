6932ef73e3bf75fdcfb1ed312c9f4092ec42fef7
cross-ts <ts.kouhei@gmail.com>
2024-12-10 23:26:59 +0900

    [17056] 割り当て待ちのrideを適当に埋めるようにする

--- webapp/php/src/Handlers/Internal/GetMatching.php
+++ webapp/php/src/Handlers/Internal/GetMatching.php
@@ -21,38 +21,39 @@ class GetMatching extends AbstractHttpHandler
         ServerRequestInterface $request,
         ResponseInterface $response,
     ): ResponseInterface {
-        // MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
-        $stmt = $this->db->prepare('SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1');
+        $stmt = $this->db->prepare('SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at');
         $stmt->execute();
-        $ride = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if (!$ride) {
+        $waitingRides = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if (count($waitingRides) === 0) {
             return $this->writeNoContent($response);
         }
-        for ($i = 0; $i < 10; $i++) {
-            $stmt = $this->db->prepare(
-                'SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1'
-            );
-            $stmt->execute();
-            $matched = $stmt->fetch(PDO::FETCH_ASSOC);
-            if (!$matched) {
+
+        foreach ($waitingRides as $ride) {
+            for ($i = 0; $i < 10; $i++) {
+                $stmt = $this->db->prepare(
+                    'SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1'
+                );
+                $stmt->execute();
+                $matched = $stmt->fetch(PDO::FETCH_ASSOC);
+                if (!$matched) {
+                    return $this->writeNoContent($response);
+                }
+                $stmt = $this->db->prepare(
+                    'SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE'
+                );
+                $stmt->execute([$matched['id']]);
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                $empty = $result['COUNT(*) = 0'];
+                if ($empty) {
+                    break;
+                }
+            }
+            if (!$empty) {
                 return $this->writeNoContent($response);
             }
-            $stmt = $this->db->prepare(
-                'SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE'
-            );
-            $stmt->execute([$matched['id']]);
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            $empty = $result['COUNT(*) = 0'];
-            if ($empty) {
-                break;
-            }
+            $stmt = $this->db->prepare('UPDATE rides SET chair_id = ? WHERE id = ? AND chair_id IS NULL');
+            $stmt->execute([$matched['id'], $ride['id']]);
         }
-        if (!$empty) {
-            return $this->writeNoContent($response);
-        }
-        $stmt = $this->db->prepare('UPDATE rides SET chair_id = ? WHERE id = ? AND chair_id IS NULL');
-        $stmt->execute([$matched['id'], $ride['id']]);
         return $this->writeNoContent($response);
     }
 }
